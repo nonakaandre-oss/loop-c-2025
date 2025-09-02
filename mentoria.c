d011db025b0dcda7be2bb86a5aea58cd589cfd9d
ANDRE NONAKA MADEIRA <nonaka.andre@gmail.com>
2025-09-02 17:32:57 +0000

    mentoria

---
+++ mentoria.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int codigo;
+    float nota;
+
+    codigo = 1;
+    nota = 8.5;
+    printf("A nota do aluno %d foi %.2f" , codigo , nota);
+
+    printf("%d", codigo);
+
+    return 0 ;
+}
